9f65154cb771ccc2c8409fd6f53da157a29d601c
Isaac Lyons <isaac@snowskeleton.net>
2022-11-12 17:17:12 -0500

    Minor renaming.

--- src/aaxconverter/book.py
+++ src/aaxconverter/book.py
@@ -1,7 +1,6 @@
 import hashlib
 import json
 from binascii import hexlify
-from dataclasses import dataclass, field
 from os import path
 
 from Crypto.Cipher import AES
@@ -10,38 +9,32 @@ from .parser import arg
 from .tinytag import MP4
 
 fixedKey = bytes.fromhex('77214d4b196a87cd520045fd20a51d67')
-@dataclass
+
+
 class Book():
-
-    infile: str
-    iv: str = field(init=False)
-    key: str = field(init=False)
-    title: str = field(init=False)
-    outfile: str = field(init=False)
-    description: str = field(init=False)
-
-    def __post_init__(self):
-        self.tags = MP4.get(self.infile, encoding='MP4')
-        self.title = self.tags.title.replace(' (Unabridged)', '')
+    def __init__(self, infile):
+        self.infile = infile
+        tags = MP4.get(self.infile, encoding='MP4')
+        self.title = tags.title.replace(' (Unabridged)', '')
         self.outfile = self.title + '.m4a'
 
         try:
-            self.description = self.tags.extra['description']
+            self.description = tags.extra['description']
         except KeyError:
-            self.description = self.tags.comment
+            self.description = tags.comment
 
         if '.aaxc' not in self.infile:
-            self.key, self.iv = deriveKeyAndIV(self)
+            self.key, self.iv = deriveKeyIV(tags)
         else:
             voucher = self.infile.replace('.aaxc', '.voucher')
             if not path.exists(voucher):
                 raise FileNotFoundError(
                     f"Oops, {self.infile} and {voucher} not together.")
 
-            self.key, self.iv = aaxcExtrasFrom(voucher)
+            self.key, self.iv = pullKeyIVFrom(voucher)
 
 
-def aaxcExtrasFrom(voucher):
+def pullKeyIVFrom(voucher):
     with open(voucher, 'r') as file:
         voucherDict = json.loads(file.read())
         key = voucherDict['content_license']['license_response']['key']
@@ -49,8 +42,8 @@ def aaxcExtrasFrom(voucher):
         return key, iv
 
 
-def deriveKeyAndIV(book: Book) -> str:
-    adrmBlob = book.tags.adrmBlob
+def deriveKeyIV(tags) -> str:
+    adrmBlob = tags.adrmBlob
     _bytes = arg('bytes')
     hexbytes = bytes.fromhex(_bytes)
 
@@ -61,7 +54,7 @@ def deriveKeyAndIV(book: Book) -> str:
     im_key = im_key[:16]
     calculatedChecksum = crypt(im_key, im_iv)
 
-    if calculatedChecksum != book.tags.checksum:
+    if calculatedChecksum != tags.checksum:
         raise AssertionError('Computed checksum != file checksum. '
                              'Either the activation bytes are incorrect, '
                              'or the audio file is invalid/corrupt.')
