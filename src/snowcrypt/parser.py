d29dbaa21f8ec15e2f6d16ae98e34bb3fd4399f5
Isaac Lyons <isaac@snowskeleton.net>
2022-11-14 15:26:12 -0500

    Remove unused arguments

--- src/snowcrypt/parser.py
+++ src/snowcrypt/parser.py
@@ -10,33 +10,12 @@ add(
     help='activation bytes to decrypt AAX files',
     default=None,
 )
-add('--dir', '-d',
-    dest='outputDir',
-    action='store',
-    default='.',
-    help='Directy in which to place the output file',
-    )
 add(
     'input',
     action='store',
     type=str,
     help='File to be converted',
     default='',
-)
-add(
-    '--output', '-o',
-    dest='output',
-    type=str,
-    action='store',
-    help='Output filename. Accepts pattern or explicit string. Defaults to filename without junk.',
-    default='output',
-)
-add(
-    '--overwrite',
-    dest='overwrite',
-    choices=['y', 'n'],
-    help='Use default option for all unset values',
-    default='y',
 )
 args, unknown = parser.parse_args()
 
