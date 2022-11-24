0aeb28985504281bfda3cc66e9dd522f0614ff0f
Isaac Lyons <isaac@snowskeleton.net>
2022-11-23 19:24:40 -0500

    Proved to be twice as fast as FFMPEG

--- src/tests/test__decrypt.py
+++ src/tests/test__decrypt.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from ..snowcrypt.snowcrypt import decrypt_aaxc as newcrypt
 from ..snowcrypt.oldcrypt import decrypt_aaxc as oldcrypt
 import time
@@ -12,7 +13,7 @@ def now():
     return time.perf_counter_ns()
 
 
-def _some(func, args: list):
+def run(func, args: list):
     start = now()
     func(*args)
     end = now()
@@ -20,45 +21,59 @@ def _some(func, args: list):
 
 
 def race(funcs: list[dict], laps: int):
-    elap = []
+    elap1 = []
     elap2 = []
     one = funcs[0]
     two = funcs[1]
 
     for _ in range(laps):
-        elap.append(_some(one['func'], one['args']))
-        elap2.append(_some(two['func'], two['args']))
+        elap1.append(run(one['func'], one['args']))
+        elap2.append(run(two['func'], two['args']))
 
-    return avg(elap), avg(elap2)
+    return avg(elap1), avg(elap2)
 
 
 class MyTestCases(unittest.TestCase):
     def test__decrypt_local(self):
-
-        contestents = [
-            {
-                # 'func': oldcrypt,
-                'func': newcrypt,
-                'args': StormAAXC,
-                # 'args': EsperoAAX,
-            },
-            {
-                'func': oldcrypt,
-                # 'func': newcrypt,
-                'args': StormAAXC,
-                # 'args': EsperoAAX,
-            }
-        ]
-        one, two = race(contestents, 20)
-        print('new: ', str(one))
-        print('old: ', str(two))
-        # print('new: ', str(one)[:3])
-        # print('old: ', str(two)[:3])
+        contestents = [{
+            'func': os.system,
+            'args': scSystemStormAAXC,
+        }, {
+            'func': os.system,
+            'args': ffSystemStormAAXC
+        }]
+        one, two = race(contestents, 1)
+        print('snowcrypt: ', str(one))
+        print('ffmpeg   : ', str(two))
 
 
 def main():
     MyTestCases().test__decrypt_local()
 
+
+ffSystemStormAAXC = [' '.join([
+    "ffmpeg",
+    "-v", "quiet",
+    "-audible_key",
+    'edd4992ee4a03f8c83601b36468aa98b',
+    "-audible_iv",
+    '866b1fdc9fdfee5675ea40264ab78ab5',
+    "-i",
+    "The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc",
+    "-c", "copy",
+    "-y",
+    '"The Gathering Storm: Interview with the Narrators.m4a"',
+])]
+
+scSystemStormAAXC = [' '.join([
+    'python pyi_entrypoint.py',
+    'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
+    'The Gathering Storm: Interview with the Narrators.m4a',
+    '--key',
+    'edd4992ee4a03f8c83601b36468aa98b',
+    '--iv',
+    '866b1fdc9fdfee5675ea40264ab78ab5',
+])]
 
 StormAAXC = [
     'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
