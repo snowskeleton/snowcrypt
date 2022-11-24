f2988715bd698ad699edd005a5db3537925ba6dd
Isaac Lyons <isaac@snowskeleton.net>
2022-11-23 23:36:16 -0500

    move constants to separate file

---
+++ src/tests/longvars.py
@@ -0,0 +1,80 @@
+import time
+
+
+def avg(list: list):
+    try:
+        return int(sum(list) / len(list))
+    except ZeroDivisionError:
+        return 0
+
+
+def now():
+    return time.perf_counter_ns()
+
+
+def run(func, args: list):
+    start = 0
+    end = 0
+    start = now()
+    func(*args)
+    end = now()
+    return end - start
+
+
+def race(funcs: list[dict], laps: int):
+    elap1 = []
+    elap2 = []
+    one = funcs[0]
+    two = funcs[1]
+
+    for _ in range(laps):
+        elap1.append(run(one['func'], one['args']))
+        elap2.append(run(two['func'], two['args']))
+
+    return avg(elap1), avg(elap2)
+
+
+ffSystemStormAAXC = [' '.join([
+    "ffmpeg",
+    "-v", "quiet",
+    "-audible_key",
+    'edd4992ee4a03f8c83601b36468aa98b',
+    "-audible_iv",
+    '866b1fdc9fdfee5675ea40264ab78ab5',
+    "-i",
+    "The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc",
+    "-c", "copy",
+    "-y",
+    '"The Gathering Storm: Interview with the Narrators.m4a"',
+])]
+
+scSystemStormAAXC = [' '.join([
+    'python pyi_entrypoint.py',
+    'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
+    'The Gathering Storm: Interview with the Narrators.m4a',
+])]
+
+ControlStormAAXC = [
+    'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
+    'The Gathering Storm: Interview with the Narrators-control.m4a',
+    'edd4992ee4a03f8c83601b36468aa98b',
+    '866b1fdc9fdfee5675ea40264ab78ab5',
+]
+StormAAXC = [
+    'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
+    'The Gathering Storm: Interview with the Narrators.m4a',
+    'edd4992ee4a03f8c83601b36468aa98b',
+    '866b1fdc9fdfee5675ea40264ab78ab5',
+]
+StormAAX = [
+    'The_Gathering_Storm_Interview_with_the_Narrators-LC_64_22050_stereo.aax',
+    'The Gathering Storm: Interview with the Narrators.m4a',
+    'f1c443f8db16304d24edc1245e278eaf',
+    '9024b63436da4986ec5ac1f56729c0e4',
+]
+EsperoAAX = [
+    'Espero_A_Silver_Ships_Novel-LC_64_22050_stereo.aax',
+    'The Gathering Storm: Interview with the Narrators.m4a',
+    '3bf90b36726bf44e540ea40ea34bd8df',
+    '8521b45c2f75c0153bbf7ce5e2e68fdd',
+]
--- src/tests/test__decrypt.py
+++ src/tests/test__decrypt.py
@@ -1,4 +1,5 @@
 import sys
+import filecmp
 import unittest
 import os
 from ..snowcrypt.snowcrypt import decrypt_aaxc as newcrypt
@@ -7,106 +8,36 @@ import time
 import cProfile
 import pstats
 
+from .longvars import *
 
-def avg(list: list):
-    return sum(list) / len(list)
+contestents = [{
+    'func': newcrypt,
+    'args': StormAAXC,
+}, {
+    'func': oldcrypt,
+    'args': ControlStormAAXC
+}]
 
-
-def now():
-    return time.perf_counter_ns()
-
-
-def run(func, args: list):
-    start = now()
-    func(*args)
-    end = now()
-    return end - start
-
-
-def race(funcs: list[dict], laps: int):
-    elap1 = []
-    elap2 = []
-    one = funcs[0]
-    two = funcs[1]
-
-    for _ in range(laps):
-        elap1.append(run(one['func'], one['args']))
-        elap2.append(run(two['func'], two['args']))
-
-    return int(avg(elap1)), int(avg(elap2))
+file1 = contestents[0]['args'][1]
+file2 = contestents[1]['args'][1]
 
 
 class MyTestCases(unittest.TestCase):
     def test__decrypt_local(self):
-        contestents = [{
-            # 'func': os.system,
-            'func': newcrypt,
-            'args': StormAAXC,
-            # 'args': scSystemStormAAXC,
-        }, {
-            # 'func': os.system,
-            'func': oldcrypt,
-            'args': StormAAXC
-            # 'args': ffSystemStormAAXC
-        }]
         # profile = cProfile.Profile()
         # profile.runcall(run, contestents[0]['func'], contestents[0]['args'])
         # ps = pstats.Stats(profile)
         # ps.print_stats()
-        num = 0
-        run(contestents[num]['func'], contestents[num]['args'])
-        sys.exit()
-        one, two = race(contestents, 15)
+        # sys.exit()
+        # num = 0
+        # print(run(contestents[num]['func'], contestents[num]['args']))
+        one, two = race(contestents, 10)
         print('new : ', str(one)[:3])
         print('old : ', str(two)[:3])
-        assert len(str(one)) == len(str(two))
-        # print('new : ', str(one))
-        # print('old : ', str(two))
-        # print('snowcrypt: ', str(one))
-        # print('ffmpeg   : ', str(two))
-        # num = 0
-        # run(contestents[num]['func'], contestents[num]['args'])
+        print('new : ', str(one))
+        print('old : ', str(two))
+        assert filecmp.cmp(file1, file2)
 
 
 def main():
     MyTestCases().test__decrypt_local()
-
-
-ffSystemStormAAXC = [' '.join([
-    "ffmpeg",
-    "-v", "quiet",
-    "-audible_key",
-    'edd4992ee4a03f8c83601b36468aa98b',
-    "-audible_iv",
-    '866b1fdc9fdfee5675ea40264ab78ab5',
-    "-i",
-    "The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc",
-    "-c", "copy",
-    "-y",
-    '"The Gathering Storm: Interview with the Narrators.m4a"',
-])]
-
-scSystemStormAAXC = [' '.join([
-    'python pyi_entrypoint.py',
-    'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
-    'The Gathering Storm: Interview with the Narrators.m4a',
-])]
-
-StormAAXC = [
-    'The_Gathering_Storm_Interview_with_the_Narrators-AAX_22_64.aaxc',
-    'The Gathering Storm: Interview with the Narrators.m4a',
-    'edd4992ee4a03f8c83601b36468aa98b',
-    '866b1fdc9fdfee5675ea40264ab78ab5',
-]
-StormAAX = [
-    'The_Gathering_Storm_Interview_with_the_Narrators-LC_64_22050_stereo.aax',
-    'The Gathering Storm: Interview with the Narrators.m4a',
-    'f1c443f8db16304d24edc1245e278eaf',
-    '9024b63436da4986ec5ac1f56729c0e4',
-]
-EsperoAAX = [
-    'Espero_A_Silver_Ships_Novel-LC_64_22050_stereo.aax',
-    'The Gathering Storm: Interview with the Narrators.m4a',
-    '3bf90b36726bf44e540ea40ea34bd8df',
-    '8521b45c2f75c0153bbf7ce5e2e68fdd',
-]
