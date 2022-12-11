e23a362ff84dd09b278c9a9b1ef99edb4eb32771
Isaac Lyons <isaac@snowskeleton.net>
2022-12-10 23:18:58 -0500

    variety

--- src/tests/test__key_derivation.py
+++ src/tests/test__key_derivation.py
@@ -7,34 +7,49 @@ from io import BytesIO
 
 
 class MyTestCases(unittest.TestCase):
-    def test__key_derivation(self):
-        file = BytesIO()
-        im_key = _sha(FIXEDKEY, bytes.fromhex(TEST_BYTES))
-        true_iv = _sha(FIXEDKEY, im_key, bytes.fromhex(TEST_BYTES))[:16]
-        true_key = im_key[:16]
-
-        # ADRM tag and length
-        tag = '6164726d' + '00000038000000'
-        offset = ADRM_START - (int(len(tag) / 2) + 1)
-        file.seek(offset)
-        file.write(bytes.fromhex(tag))
-
-        cipher = newAES(true_key, MODE_CBC, iv=true_iv)
-        adrm = 'f' * ADRM_LENGTH + TEST_BYTES
-        adrm = bytes.fromhex(_swapEndian(adrm))
-        file.write(bytes.fromhex('01') + cipher.encrypt(_pad_16(adrm)))
-
-        # checksum
-        file.write(bytes.fromhex('00000000' '00000001' '00000000'))
-        file.write(_sha(true_key, true_iv))
-
-        # this is the actual test. should return no errors.
-        test_key, test_iv = deriveKeyIV(file, TEST_BYTES)
-        self.assertEqual(true_key, test_key)
-        self.assertEqual(true_iv, test_iv)
-
+    def test__key_derivation_with_good_key(self):
+        file = create_test_file(TEST_BYTES)
+        deriveKeyIV(file, TEST_BYTES)
         file.close()
 
+    def test__key_derivation_with_bad_key(self):
+        from ..snowcrypt.localExceptions import CredentialMismatch
+        garbage = 'abc123ff'  # random hex string, different than TEST_BYTES
+        file = create_test_file(garbage)
+        self.assertRaises(CredentialMismatch, deriveKeyIV, file, TEST_BYTES)
+        file.close()
+
+
+def create_test_file(activation_bytes: str) -> BytesIO:
+    """creates BytesIO() object and returns it.
+    Be sure to call obj.close() when you're done!
+
+    Args:
+        activation_bytes (str): seed
+
+    Returns:
+        BytesIO: adrm example
+    """
+    file = BytesIO()
+    im_key = _sha(FIXEDKEY, bytes.fromhex(activation_bytes))
+    true_iv = _sha(FIXEDKEY, im_key, bytes.fromhex(activation_bytes))[:16]
+    true_key = im_key[:16]
+
+    # ADRM tag and length
+    tag = '6164726d' + '00000038000000'
+    offset = ADRM_START - (int(len(tag) / 2) + 1)
+    file.seek(offset)
+    file.write(bytes.fromhex(tag))
+
+    cipher = newAES(true_key, MODE_CBC, iv=true_iv)
+    adrm = 'f' * ADRM_LENGTH + activation_bytes
+    adrm = bytes.fromhex(_swapEndian(adrm))
+    file.write(bytes.fromhex('01') + cipher.encrypt(_pad_16(adrm)))
+
+    # checksum
+    file.write(bytes.fromhex('00000000' '00000001' '00000000'))
+    file.write(_sha(true_key, true_iv))
+    return file
 
 if __name__ == '__main__':
     unittest.main()
