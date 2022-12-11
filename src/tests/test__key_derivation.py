0d520f23d04cf5999e88c32198d64c0a2dc7e6b6
Isaac Lyons <isaac@snowskeleton.net>
2022-12-10 22:36:06 -0500

    style

--- src/tests/test__key_derivation.py
+++ src/tests/test__key_derivation.py
@@ -7,31 +7,31 @@ from .constants import *
 
 
 class MyTestCases(unittest.TestCase):
-  def test__decrypt_local(self):
-    create_test_file(TEST_SAMPLE_FILE)
-    with open(TEST_SAMPLE_FILE, 'rb') as file:
-      deriveKeyIV(file, TEST_BYTES)
+    def test__decrypt_local(self):
+        create_test_file(TEST_SAMPLE_FILE)
+        with open(TEST_SAMPLE_FILE, 'rb') as file:
+            deriveKeyIV(file, TEST_BYTES)
 
 
 def create_test_file(filename):
     with open(filename, 'wb') as inStream:
-      im_key = _sha(FIXEDKEY, bytes.fromhex(TEST_BYTES))
-      iv = _sha(FIXEDKEY, im_key, bytes.fromhex(TEST_BYTES))[:16]
-      key = im_key[:16]
+        im_key = _sha(FIXEDKEY, bytes.fromhex(TEST_BYTES))
+        iv = _sha(FIXEDKEY, im_key, bytes.fromhex(TEST_BYTES))[:16]
+        key = im_key[:16]
 
-      # ADRM tag and length
-      tag = '6164726d' + '00000038000000'
-      offset = ADRM_START - (int(len(tag) / 2) + 1)
-      inStream.seek(offset)
-      inStream.write(bytes.fromhex(tag))
+        # ADRM tag and length
+        tag = '6164726d' + '00000038000000'
+        offset = ADRM_START - (int(len(tag) / 2) + 1)
+        inStream.seek(offset)
+        inStream.write(bytes.fromhex(tag))
 
-      cipher = newAES(key, MODE_CBC, iv=iv)
-      adrm = bytes.fromhex(_swapEndian('f' * ADRM_LENGTH + TEST_BYTES))
-      inStream.write(bytes.fromhex('01') + cipher.encrypt(_pad_16(adrm)))
+        cipher = newAES(key, MODE_CBC, iv=iv)
+        adrm = bytes.fromhex(_swapEndian('f' * ADRM_LENGTH + TEST_BYTES))
+        inStream.write(bytes.fromhex('01') + cipher.encrypt(_pad_16(adrm)))
 
-      # checksum
-      inStream.write(bytes.fromhex('00000000' '00000001' '00000000'))
-      inStream.write(_sha(key, iv))
+        # checksum
+        inStream.write(bytes.fromhex('00000000' '00000001' '00000000'))
+        inStream.write(_sha(key, iv))
 
 
 def _pad_16(data: bytes, length: int = 16) -> bytes:
