eb6b21caa31fe0415ed450ad2378fd6862c858fe
Isaac Lyons <isaac@snowskeleton.net>
2022-12-10 23:00:14 -0500

    restructure to read better

--- src/tests/test__key_derivation.py
+++ src/tests/test__key_derivation.py
@@ -9,30 +9,31 @@ from io import BytesIO
 class MyTestCases(unittest.TestCase):
     def test__key_derivation(self):
         file = BytesIO()
-        create_test_adrm_file(file)
-        deriveKeyIV(file, TEST_BYTES)
+        im_key = _sha(FIXEDKEY, bytes.fromhex(TEST_BYTES))
+        true_iv = _sha(FIXEDKEY, im_key, bytes.fromhex(TEST_BYTES))[:16]
+        true_key = im_key[:16]
+
+        # ADRM tag and length
+        tag = '6164726d' + '00000038000000'
+        offset = ADRM_START - (int(len(tag) / 2) + 1)
+        file.seek(offset)
+        file.write(bytes.fromhex(tag))
+
+        cipher = newAES(true_key, MODE_CBC, iv=true_iv)
+        adrm = 'f' * ADRM_LENGTH + TEST_BYTES
+        adrm = bytes.fromhex(_swapEndian(adrm))
+        file.write(bytes.fromhex('01') + cipher.encrypt(_pad_16(adrm)))
+
+        # checksum
+        file.write(bytes.fromhex('00000000' '00000001' '00000000'))
+        file.write(_sha(true_key, true_iv))
+
+        # this is the actual test. should return no errors.
+        test_key, test_iv = deriveKeyIV(file, TEST_BYTES)
+        self.assertEqual(true_key, test_key)
+        self.assertEqual(true_iv, test_iv)
+
         file.close()
-
-
-def create_test_adrm_file(file):
-    im_key = _sha(FIXEDKEY, bytes.fromhex(TEST_BYTES))
-    iv = _sha(FIXEDKEY, im_key, bytes.fromhex(TEST_BYTES))[:16]
-    key = im_key[:16]
-
-    # ADRM tag and length
-    tag = '6164726d' + '00000038000000'
-    offset = ADRM_START - (int(len(tag) / 2) + 1)
-    file.seek(offset)
-    file.write(bytes.fromhex(tag))
-
-    cipher = newAES(key, MODE_CBC, iv=iv)
-    adrm = 'f' * ADRM_LENGTH + TEST_BYTES
-    adrm = bytes.fromhex(_swapEndian(adrm))
-    file.write(bytes.fromhex('01') + cipher.encrypt(_pad_16(adrm)))
-
-    # checksum
-    file.write(bytes.fromhex('00000000' '00000001' '00000000'))
-    file.write(_sha(key, iv))
 
 
 if __name__ == '__main__':
